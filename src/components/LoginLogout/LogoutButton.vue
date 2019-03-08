6b30dc4bcae16c9f33d92ad11943d33649119c1b
MuesLee <stimo@gmx.net>
2019-03-08 08:58:43 +0100

    LogoutButton refactor

---
+++ src/components/LoginLogout/LogoutButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    <v-btn @click="logout()">Logout</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import store from "@/store/index";
+
+
+@Component({
+  components: {
+  },
+  methods: {
+    logout: () => store.dispatch('logoutAction')
+  }
+
+})
+export default class LogoutButton extends Vue {}
+
+
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="login">
     <LoginForm v-if="!authed" ></LoginForm>
-    <v-btn v-if="authed" @click="logout()">Logout</v-btn>
+    <LogoutButton v-if="authed" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import LoginForm from '@/components/LoginForm.vue';
+import LoginForm from '@/components/LoginLogout/LoginForm.vue';
+import LogoutButton from '@/components/LoginLogout/LogoutButton.vue';
 import authService from "@/services/AuthService";
 import store from "@/store/index";
 
 
 @Component({
   components: {
-    LoginForm,
+    LoginForm, LogoutButton
   },
   computed: {
     authed: () => authService.userIsAuthenticated()
@@ -25,10 +26,5 @@ import store from "@/store/index";
 
 })
 export default class LoginView extends Vue {}
-
-function userIsAuthed()
-{
-  return ;
-}
 
 </script>
