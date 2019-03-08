636ec706fd65227c91e5097d3cc79a9a5d0c2769
MuesLee <stimo@gmx.net>
2019-03-08 09:25:41 +0100

    Toolbar refactoring with link

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-flex xs12>
-        <v-img :src="require('./assets/logo.png')" class="my-3" contain height="50"></v-img>
-      </v-flex>
-    </v-toolbar>
+    <PantryToolbar/>
     <v-content>
         <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
-<script>
+<script lang="ts">
+import PantryToolbar from '@/components/PantryToolbar.vue';
 
 export default {
   name: 'App',
   components: {
+    PantryToolbar
   },
   data() {
     return {
---
+++ src/components/PantryToolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar class="text-xs-center">
+    <v-flex xs12>
+      <v-avatar :size="50" color="grey lighten-4"> 
+        <router-link to="home">
+          <v-img :src="require('@/assets/logo.png')" class="my-3" contain height="50" width="50"/>
+        </router-link>
+      </v-avatar>
+    </v-flex>
+  </v-toolbar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+  methods: {}
+})
+export default class PantryToolbar extends Vue {}
+</script>
