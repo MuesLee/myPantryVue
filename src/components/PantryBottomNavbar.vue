5be11415e849aad5f8e3cbcd6af4e6c890df042f
MuesLee <stimo@gmx.net>
2019-03-09 16:06:58 +0100

    router to navbar binding

--- src/components/PantryBottomNavbar.vue
+++ src/components/PantryBottomNavbar.vue
@@ -1,51 +1,43 @@
 <template>
-    <v-bottom-nav
-      :value=true
-      absolute
-      :mandatory=true
-      color="transparent"
-    >
-      <v-btn
-        color="primary"
-        flat
-        @click="routeTo('shoppingList')"
-      >
-        <span>Einkaufen</span>
-        <v-icon>shopping_basket</v-icon>
-      </v-btn>
+  <v-bottom-nav
+    :active.sync="activeRouteName"
+    :value="true"
+    absolute
+    :mandatory="true"
+    color="transparent"
+  >
+    <v-btn color="primary" flat @click="routeTo('shoppingList')" value="shoppingList">
+      <span>Einkaufen</span>
+      <v-icon>shopping_basket</v-icon>
+    </v-btn>
 
-      <v-btn
-        color="primary"
-        flat
-         @click="routeTo('home')"
-      >
-        <span>Übersicht</span>
-        <v-icon>view_agenda</v-icon>
-      </v-btn>
+    <v-btn color="primary" flat @click="routeTo('home')" value="home">
+      <span>Übersicht</span>
+      <v-icon>view_agenda</v-icon>
+    </v-btn>
 
-      <v-btn
-        color="primary"
-        flat
-        @click="routeTo('pantry')"
-      >
-        <span>Vorrat</span>
-        <v-icon>home</v-icon>
-      </v-btn>
-
-    </v-bottom-nav>
+    <v-btn color="primary" flat @click="routeTo('pantry')" value="pantry">
+      <span>Vorrat</span>
+      <v-icon>home</v-icon>
+    </v-btn>
+  </v-bottom-nav>
 </template>
 
 <script lang='ts'>
-import { Component, Vue } from "vue-property-decorator";
-import router from '@/router';
+import { Component, Vue, Prop } from "vue-property-decorator";
+import router from "@/router";
 
 @Component({
   components: {},
-  methods: { routeTo: target => router.push(target) },
-  data () {
-      return {
-      }
-    }
+  methods: { routeTo: target => router.push(target) }
 })
-export default class PantryBottomNavbar extends Vue {}
+export default class PantryBottomNavbar extends Vue {
+  activeRouteName = "home";
+
+  mounted() {
+    router.afterEach((to, from) => {
+      this.activeRouteName = !!to.name ? to.name : "home";
+    });
+  }
+}
 </script>
